eb0d610baa127341b1c004fe5f305e562044ec79
SudeepPattanshetty <sudeeppattanshetty161@gmail.com>
2025-08-07 12:47:45 +0530

    working add items

--- 15_Local_Storage/script.js
+++ 15_Local_Storage/script.js
@@ -1,3 +1,29 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+        text,
+        done: false
+    };
+
+    items.push(item);
+    populateList(items, itemsList)
+    this.reset();
+}
+
+function populateList(plates = [], plateList) {
+    plateList.innerHTML = plates.map((plate, i) => {
+        return `
+        <li>
+            <input type="checkbox" data-index=${1} id="item${1}" ${plate.done ? "checked" : ""}/>
+            <label for="item${1}">${plate.text}</label>
+        </li>
+        `
+    }).join('')
+}
+
+addItems.addEventListener('submit', addItem);
